f8cb9de97bf4fcca5f7fc2a087f2f2ed55b7773a
Michael Lamb <mlamb892@gmail.com>
2019-07-22 16:37:00 -0600

    Add carousel

--- src/components/AntCarousel/AntCarousel.js
+++ src/components/AntCarousel/AntCarousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import Carousel from "react-native-snap-carousel";
 
 import AntImage from "../AntImage";
 
@@ -7,9 +8,19 @@ class AntCarousel extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <AntImage />
-        <AntImage />
-        <AntImage />
+        <Carousel
+          layout={"default"}
+          data={[1, 2, 3]}
+          loop={true}
+          firstItem={1}
+          loopClonesPerSide={100}
+          renderItem={() => <AntImage />}
+          sliderWidth={350}
+          itemWidth={120}
+          autoplay={true}
+          autoplayDelay={0}
+          autoplayInterval={100}
+        />
       </View>
     );
   }
