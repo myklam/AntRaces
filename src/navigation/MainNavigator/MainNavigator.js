d2a94c501590164389152695d70a87940fd3362b
Michael Lamb <mlamb892@gmail.com>
2019-07-22 11:36:58 -0600

    Add MainNavigator

---
+++ src/navigation/MainNavigator/MainNavigator.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class MainNavigator extends Component {
+  render() {
+    return (
+      <View>
+        <Text>MainNavigator</Text>
+      </View>
+    );
+  }
+}
+
+export default MainNavigator;
