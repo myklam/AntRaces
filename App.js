8c3d1920aa0226d799fa33defaf1888be872bf7f
Michael Lamb <mlamb892@gmail.com>
2019-07-22 13:24:56 -0600

    Add and configure apollo

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import MainNavigator from "./src/navigation/MainNavigator";
+
+const client = new ApolloClient({
+  uri: "https://antserver-blocjgjbpw.now.sh/graphql"
+});
+
 const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <MainNavigator />
-    </SafeAreaView>
+    <ApolloProvider client={client}>
+      <SafeAreaView style={styles.container}>
+        <MainNavigator />
+      </SafeAreaView>
+    </ApolloProvider>
   );
 };
 
